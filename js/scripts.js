7760b5b9ea3c19845f0c8e7305477ab0e9617ef9
mellowmorgan <morgan.waites@gmail.com>
2021-10-19 11:02:53 -0700

    DEBUG: Fix togglek
    Co-authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-  $(".clickable").click(function(){
-    $(".hidden").toggle;
+
+$(document).ready(function() {
+  $(".clickable-1").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable-2").click(function() {
+    $("#walrus-showing-2").toggle();
+    $("#walrus-hidden-2").toggle();
   });
 });
